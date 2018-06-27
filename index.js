01128cbad9ac6b8f18f1cae535a2e09d76a9ee75
rachit5 <r.lohani555@gmail.com>
2018-06-27 13:20:10 +0530

    second setup commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Main from './src/Main';
 
-AppRegistry.registerComponent('WMW', () => App);
+AppRegistry.registerComponent('WMW', () => Main);
